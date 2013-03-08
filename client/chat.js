fdeecc5e548ab83a120dad529448a2ac93a3afbd
Tom Coleman <tom@thesnail.org>
2013-03-08 13:54:54 +1100

    Step 4: Moved the fixture data over to a collection, display.

--- client/chat.js
+++ client/chat.js
@@ -1,23 +1,15 @@
 Meteor.Router.add({
   '/': 'home',
-  '/rooms/:id': 'room'
+  '/rooms/:id': function(id) {
+    Session.set('currentRoomId', id);
+    return 'room'
+  }
 });
 
-var rooms = [
-  {name: 'Meteor Talk', members: 3, last_activity: '1 minute ago',
-    messages: [
-      {author: 'Tom', text: 'Hi there Sacha!'},
-      {author: 'Sacha', text: 'Hey Tom, how are you?'},
-      {author: 'Tom', text: 'Good thanks!'},
-    ]},
-  {name: 'Meteor Development', members: 2, last_activity: '5 minutes ago'},
-  {name: 'Meteor Core', members: 0, last_activity: '3 days ago'}
-];
-
 Template.rooms.helpers({
-  rooms: rooms
+  rooms: function() { return Rooms.find(); }
 });
 
 Template.room.helpers({
-  room: rooms[0]
-});
+  room: function() { return Rooms.findOne(Session.get('currentRoomId')); }
+})
