281e232d2880caa9bccbc48b261e4f496117aaae
Tom Coleman <tom@thesnail.org>
2013-03-08 13:28:41 +1100

    Step 2: Added a basic room route and linked to it in the table.

--- client/chat.js
+++ client/chat.js
@@ -1,3 +1,8 @@
+Meteor.Router.add({
+  '/': 'home',
+  '/rooms/:id': 'room'
+});
+
 var rooms = [
   {name: 'Meteor Talk', members: 3, last_activity: '1 minute ago'},
   {name: 'Meteor Development', members: 2, last_activity: '5 minutes ago'},
