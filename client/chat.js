fc1272187f686fc90bf10bd35b935cf4db7885c2
Tom Coleman <tom@thesnail.org>
2013-03-08 13:04:44 +1100

    Step 1: Created basic JS back wire-frame

---
+++ client/chat.js
@@ -0,0 +1,9 @@
+var rooms = [
+  {name: 'Meteor Talk', members: 3, last_activity: '1 minute ago'},
+  {name: 'Meteor Development', members: 2, last_activity: '5 minutes ago'},
+  {name: 'Meteor Core', members: 0, last_activity: '3 days ago'}
+]
+
+Template.rooms.helpers({
+  rooms: rooms
+});
