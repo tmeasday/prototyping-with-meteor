3c5f59299376350008267745a67074a3339e088e
Tom Coleman <tom@thesnail.org>
2013-03-08 13:52:53 +1100

    Step 3: Display some simple JS backed data for a room.

--- client/chat.js
+++ client/chat.js
@@ -4,11 +4,20 @@ Meteor.Router.add({
 });
 
 var rooms = [
-  {name: 'Meteor Talk', members: 3, last_activity: '1 minute ago'},
+  {name: 'Meteor Talk', members: 3, last_activity: '1 minute ago',
+    messages: [
+      {author: 'Tom', text: 'Hi there Sacha!'},
+      {author: 'Sacha', text: 'Hey Tom, how are you?'},
+      {author: 'Tom', text: 'Good thanks!'},
+    ]},
   {name: 'Meteor Development', members: 2, last_activity: '5 minutes ago'},
   {name: 'Meteor Core', members: 0, last_activity: '3 days ago'}
-]
+];
 
 Template.rooms.helpers({
   rooms: rooms
+});
+
+Template.room.helpers({
+  room: rooms[0]
 });
