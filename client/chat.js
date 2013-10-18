22270e828eddf8d1ed129396fb67dce4ab7cb1a8
Mike Cunneen <mike@cunneen.biz>
2013-10-18 12:59:47 +0800

    Updated version of chat tutorial step 4, modified to work with Meteor 0.6.6.1 and IronRouter.

--- client/chat.js
+++ client/chat.js
@@ -1,15 +1,58 @@
-Meteor.Router.add({
-  '/': 'home',
-  '/rooms/:id': function(id) {
-    Session.set('currentRoomId', id);
-    return 'room'
-  }
+Router.configure({
+   layout: 'layout',
+   notFoundTemplate: 'notFound',
+   loadingTemplate: 'loading',
+   renderTemplates: {
+      /* Any templates to always render can be specified here */
+//      'home': {to: 'homeYield'}
+
+   }
 });
 
-Template.rooms.helpers({
-  rooms: function() { return Rooms.find(); }
+Router.map(function() {
+   this.route('home', {
+      path: '/',
+      template: 'layout',
+      renderTemplates: {
+         'home': {to: 'homeYield'}
+      }
+   });
+   this.route('room', {
+      path: '/rooms/:_id',
+      data: function() {
+         return Rooms.findOne({"_id": this.params._id});
+      },
+      template: 'layout', // use the 'layout' template for this route
+      renderTemplates: {
+         'room': {to: 'roomYield'} // render the 'room' template to the 'roomYield' yield.
+      }
+  });
 });
 
-Template.room.helpers({
-  room: function() { return Rooms.findOne(Session.get('currentRoomId')); }
-})
+if (Meteor.isClient) {
+   Meteor.startup(function() {
+      // code to run on client when dom is ready
+   });
+
+
+   Template.rooms.helpers({
+      rooms: function() {
+         return Rooms.find();
+      }
+   });
+
+   Template.room.helpers({
+      room: function() {
+         return Rooms.findOne(Session.get('currentRoomId'));
+      }
+   });
+
+   Template.rooms.events({
+   });
+}
+
+if (Meteor.isServer) {
+   Meteor.startup(function() {
+      // code to run on server at startup
+   });
+}
